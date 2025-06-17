3bffaef5d1ab4d2068e71849ea487d394bc7cdcc
Labbey Ammar Ahmed <141624572+ammar-codeable@users.noreply.github.com>
2025-06-17 21:03:51 +0530

    chore: log running port number to stdout

--- src/lib.rs
+++ src/lib.rs
@@ -12,12 +12,14 @@ async fn subscribe() -> HttpResponse {
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
-App::new()
-.route("/health_check", web::get().to(health_check))
+        App::new()
+            .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
     })
-        .listen(listener)?
-        .run();
+    .listen(listener.try_clone()?)?
+    .run();
+
+    println!("Server running on {}", listener.local_addr().unwrap());
 
     Ok(server)
 }
