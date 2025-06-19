625ad64e29f90f3721b79a7086212f4662f715b0
Labbey Ammar Ahmed <141624572+ammar-codeable@users.noreply.github.com>
2025-06-19 20:43:55 +0530

    feat: add logging for new subscriber process in the subscribe function

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -10,6 +10,19 @@ pub struct FormData {
 }
 
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    let request_id = Uuid::new_v4();
+    log::info!(
+        "request_id {} - Adding '{}' '{}' as a new subscriber.",
+        request_id,
+        form.email,
+        form.name
+    );
+
+    log::info!(
+        "request_id {} - Saving new subscriber details in the database",
+        request_id
+    );
+    
     match sqlx::query!(
         r#"
     INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -23,9 +36,12 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
     .execute(pool.as_ref())
     .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            log::info!("New subscription details have been saved");
+            HttpResponse::Ok().finish()
+        }
         Err(e) => {
-            println!("Failed to execute query: {}", e);
+            log::error!("Failed to execute query: {:?}", e);
             HttpResponse::InternalServerError().finish()
         }
     }
